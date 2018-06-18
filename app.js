4a2172d69b805bf7d751b169197efe21c57b2128
Rajesh Botchu <rajessrao@gmail.com>
2018-06-18 11:40:54 +0530

    Port changed to 4000

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 4000, function () {
     log.info('Started the express microservice.');
 });
 
